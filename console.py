82f603d83ef8283dea3b0439ddda8b16fcd25484
taf <muzirat0@gmail.com>
2024-02-05 16:39:45 +0200

    consele infastructure

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
+        print()
+        return True
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        return None
+
+    def do_show_(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        return None
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id (save the change into the JSON file)"""
+        return None
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name"""
+        return None
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)"""
+        return None
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
