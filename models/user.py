414bb9a17e0ec02bbf06311485b6db31d3352b3d
root <root@DESKTOP-K7H8GF6>
2024-02-13 21:18:26 +0200

    user instance attrs -> class attrs

--- models/user.py
+++ models/user.py
@@ -6,10 +6,11 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """ user class """
 
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
